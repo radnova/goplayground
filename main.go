7980ae0ec78dc9480823ff11da334546593194a8
Rob Weeks <weeks@radiantnova.com>
2023-10-25 23:19:56 -0400

    branched battleship, messing with 2D arrays

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+const numRows = 11
+const numCols = 11
+
+var board [numRows][numCols]int
+
+func main() {
+	for i := 0; i < numRows; i++ {
+		for j := 0; j < numCols; j++ {
+			board[i][j] = 0
+		}
+	}
+	colHeader := "A"
+	fmt.Print(" ")
+	for i := 1; i < numCols; i++ {
+		fmt.Printf("%s ", colHeader)
+		nextRune := []rune(colHeader)[0] + 1
+		colHeader = string(nextRune)
+	}
+	fmt.Println("")
+
+	for i:= 0; i < numCols; i++ {
+		for j := 0
+	}
 }
